95398569871448547da23b24216cf1eca390fc87
Maciej Zygadło <Xsardas665@gmail.com>
2020-11-25 14:45:01 +0100

    Stage 3

--- README.md
+++ README.md
@@ -12,6 +12,10 @@ The first player that writes 3 'X's or 3 'O's in a straight line (including diag
 However, our game should show the field in an "intermediate" states too. Let's try to visualize different combinations 
 that the user will determine from the input. It is also important to think about the interface and set boundaries for 
 our field.
+#### Stage 3/5:
+It is time to learn to see the result (or the lack thereof) of the game. In this stage, you should analyze a Tic-Tac-Toe field.
+Is it already clear who's the winner or is the game not over yet? Is it a draw or an impossible combination of moves? Let's find out!
+Note that in this stage either 'X' or 'O' can start the game.
 
 ### Objectives
 #### Stage 1/5:
@@ -24,3 +28,16 @@ In this stage, you should write a program that:
 - Sets the field to a specific format, i.e. the field should start and end with ---------, 
   all lines in between should start and end with '|' symbol and everything in 
   the middle should be separated with a single space.
+  
+#### Stage 3/5 :
+In this stage your program should:
+
+- Fill the field from the input and print it as in the previous stage.
+- Find the state in which the game is at the moment and print it. Possible states:
+  - "Game not finished" when no side has a three in a row but there are still empty cells;
+  - "Draw" when no side has a three in a row and there are no empty cells left;
+  - "X wins" when the field has three Xs in a row;
+  - "O wins" when the field has three Os in a row;
+  - "Impossible" when the field has three Xs in a row as well as three Os in a row. Or the field has a lot more Xs than Os or vice versa (if the difference is 2 or more, should be 1 or 0). For this stage, consider that the game can start both with X or O.
+
+Also, you can use ' ' or '_' to print empty cells – it's up to you
--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -5,15 +5,83 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String board = scanner.nextLine();
-        printBoard(board);
+        char[] chars = scanner.nextLine().toCharArray();
+        printFiled(chars);
+        char x = 'X';
+        char o = 'O';
+        if (win(chars, x)) {
+            System.out.println("X wins");
+        } else if (win(chars, o)) {
+            System.out.println("O wins");
+        }
+        if (!hasEmpty(chars) && !win(chars, x) && !win(chars, o)) {
+            System.out.println("Draw");
+        }
+        if (hasEmpty(chars) && !win(chars, x) && !win(chars, o)) {
+            System.out.println("Game not finished");
+        }
+        if (win(chars, x) && win(chars, o) || hasDifferenceCount(chars)) {
+            System.out.println("Impossible");
+        }
     }
 
-    private static void printBoard(String s) {
-        System.out.println("---------");
-        System.out.println("| " + s.charAt(0) + " " + s.charAt(1) + " " + s.charAt(2) + " |");
-        System.out.println("| " + s.charAt(3) + " " + s.charAt(4) + " " + s.charAt(5) + " |");
-        System.out.println("| " + s.charAt(6) + " " + s.charAt(7) + " " + s.charAt(8) + " |");
-        System.out.println("---------");
+    public static void printFiled(char[] values) {
+        int length = values.length;
+        int size = (int) Math.sqrt(length);
+        for (int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+        System.out.print("\n| ");
+        for (int i = 0; i < length; i++) {
+            if (i >= size && i % size == 0) {
+                System.out.print("|\n| ");
+            }
+            System.out.print(values[i] + " ");
+        }
+        System.out.println("|");
+        for (int i = 0; i < length; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public static boolean win(char[] values, char value) {
+        boolean horizontal = (value == values[0] && value == values[1] && value == values[2])
+                ||
+                (value == values[3] && value == values[4] && value == values[5])
+                ||
+                (value == values[6] && value == values[7] && value == values[8]);
+        boolean vertical = (value == values[0] && value == values[3] && value == values[6])
+                ||
+                (value == values[1] && value == values[4] && value == values[7])
+                ||
+                (value == values[2] && value == values[5] && value == values[8]);
+        boolean diagonal = (value == values[0] && value == values[4] && value == values[8])
+                ||
+                (value == values[2] && value == values[4] && value == values[6]);
+        return horizontal || vertical || diagonal;
+    }
+
+    public static boolean hasEmpty(char[] values) {
+        for (char value : values) {
+            if (value == '_') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean hasDifferenceCount(char[] values) {
+        int x = 0;
+        int o = 0;
+        for (char value : values) {
+            if (value == 'X') {
+                x++;
+            } else if (value == 'O') {
+                o++;
+            }
+        }
+        int difference = x - o;
+        return difference >= 2 || difference <= -2;
     }
 }
